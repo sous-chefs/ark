a4db1536c302fef9117b41d2353b06c66c7903df
Bryan Berry <bryan.berry@gmail.com>
2012-10-17 14:09:21 +0200

    add setup_py_install and setup_py_build actions

--- libraries/resource_ark.rb
+++ libraries/resource_ark.rb
@@ -27,7 +27,7 @@ class Chef
       def initialize(name, run_context=nil)
         super
         @resource_name = :ark
-        @allowed_actions.push(:install, :dump, :cherry_pick, :put, :install_with_make, :configure, :setup_py)
+        @allowed_actions.push(:install, :dump, :cherry_pick, :put, :install_with_make, :configure, :setup_py_build, :setup_py_install, :setup_py)
         @action = :install
         @provider = Chef::Provider::Ark
       end
