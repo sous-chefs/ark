2c2330f15df20cab0fee08f853c7993e478f3493
Sean OMeara <someara@opscode.com>
2013-03-01 11:33:10 -0500

    porting providers to use_inline_resources LWRP DSL. test.rb converging cleaning.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,3 +3,4 @@ default['ark']['apache_mirror'] = 'http://apache.mirrors.tds.net'
 default['ark']['prefix_root'] = "/usr/local"
 default['ark']['prefix_bin'] = "/usr/local/bin"
 default['ark']['prefix_home'] = "/usr/local"
+default['ark']['tar'] = "/bin/tar"
