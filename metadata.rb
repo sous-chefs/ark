279d6e10bd238a6092fafd5ba19d9596c0d1e01d
jtimberman <joshua@opscode.com>
2013-04-04 23:50:30 -0600

    increment version for development

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "bryan.berry@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures ark"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
 %w{ debian ubuntu centos redhat fedora }.each do |os|
   supports os
