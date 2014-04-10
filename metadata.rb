c3b8400659ad292f41f33984885fe598277d4cd4
Sean OMeara <someara@opscode.com>
2014-04-10 09:56:22 -0400

    Version bump to 0.8.1 (for development)

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'bryan.berry@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures ark'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.8.0'
+version          '0.8.1'
 
 %w( debian ubuntu centos redhat fedora ).each do |os|
   supports os
