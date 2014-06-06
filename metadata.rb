12f9b4835a54c78ea57e35d5be33193fa7451f85
Christoph Kraemer <mail@christophkraemer.de>
2014-06-06 15:15:38 -0700

    [COOK-3642] Add Windows support
    Signed-off-by: Christopher Webber <cwebber@getchef.com>

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,11 @@ description      'Installs/Configures ark'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.8.3'
 
-%w( debian ubuntu centos redhat fedora ).each do |os|
+%w( debian ubuntu centos redhat fedora windows ).each do |os|
   supports os
 end
 
 recipe 'ark::default', 'Installs and configures ark'
+
+depends 'windows'
+depends '7-zip'
