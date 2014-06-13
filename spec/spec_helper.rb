0b5a245b1f7e1510ccde26283329e8af0dec17ce
Franklin Webber <franklin.webber@gmail.com>
2014-06-13 14:21:46 -0700

    Updated Spec Helper
    * Added color to the output
    * Added an alias example so I can use `describe_recipe "ark::default"`
    * The alias automatically loads the shared_context that gives me some
      helper methods like:
    
      ** chef_run - default run with no node_properties
      ** node - the chef_run's node.
      ** node_properties - blank, but can be overriden when needing to change the platform
      ** default_cookbook_attribute - lets me check for an attribute on the node

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,36 @@ require 'chefspec'
 require 'chefspec/berkshelf'
 
 at_exit { ChefSpec::Coverage.report! }
+
+RSpec.configure do |config|
+  config.color = true
+  config.alias_example_group_to :describe_recipe, :type => :recipe
+end
+
+RSpec.shared_context "recipe tests", :type => :recipe do
+
+  let(:chef_run) { ChefSpec::Runner.new(node_properties).converge(described_recipe) }
+
+  let(:node) { chef_run.node }
+
+  def node_properties
+    {}
+  end
+
+  def cookbook_recipe_names
+    described_recipe.split("::",2)
+  end
+
+  def cookbook_name
+    cookbook_recipe_names.first
+  end
+
+  def recipe_name
+    cookbook_recipe_names.last
+  end
+
+  def default_cookbook_attribute(attribute_name)
+    node[cookbook_name][attribute_name]
+  end
+
+end
