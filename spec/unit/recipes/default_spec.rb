b9d5d54d1477669216e4d3d67e9bd2a470278a73
Franklin Webber <franklin.webber@gmail.com>
2014-06-13 14:26:10 -0700

    Adding specs to the default recipe
    * tests the default recipe for all packages and default parameters
    * tests the default recipe for CentOS

---
+++ spec/unit/recipes/default_spec.rb
@@ -0,0 +1,35 @@
+require "spec_helper"
+
+describe_recipe "ark::default" do
+
+  let(:expected_core_packages) do
+    %w( libtool autoconf unzip rsync make gcc autogen )
+  end
+
+  it "installs core packages" do
+    expected_core_packages.each do |package|
+      expect(chef_run).to install_package(package)
+    end
+  end
+
+  context "sets default attributes" do
+
+    it "apache mirror" do
+      expect(default_cookbook_attribute("apache_mirror")).to eq "http://apache.mirrors.tds.net"
+    end
+
+    it "prefix root" do
+      expect(default_cookbook_attribute("prefix_root")).to eq "/usr/local"
+    end
+
+    it "prefix bin" do
+      expect(default_cookbook_attribute("prefix_bin")).to eq "/usr/local/bin"
+    end
+
+    it "prefix home" do
+      expect(default_cookbook_attribute("prefix_home")).to eq "/usr/local"
+    end
+
+  end
+
+end
