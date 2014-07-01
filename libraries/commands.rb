b190416d4d0e829d32e6fba15a93753954151757
Franklin Webber <franklin.webber@gmail.com>
2014-07-01 12:13:10 -0700

    Removing a temporary refactoring idea

--- libraries/commands.rb
+++ libraries/commands.rb
@@ -3,18 +3,6 @@ require_relative 'sevenzip_commands'
 require_relative 'unzip_commands'
 require_relative 'tar_commands'
 
-class UnpackCommander
-  def self.choose(resource)
-    if resource.run_context.node['platform_family'] == 'windows'
-      WindowsUnpacker.new(resource)
-    elsif true
-      TarUnpacker.new(resource)
-    else
-      UnzipUnpacker.new(resource)
-    end
-  end
-end
-
 class GeneralOwner
   def initialize(resource)
     @resource = resource
