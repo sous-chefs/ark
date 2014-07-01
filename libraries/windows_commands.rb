0843dcadad71819ec4b5431ded44127ac114aae9
Franklin Webber <franklin.webber@gmail.com>
2014-07-01 11:52:45 -0700

    Minor refactor of the WindowsUnpacker command

--- libraries/windows_commands.rb
+++ libraries/windows_commands.rb
@@ -11,41 +11,43 @@ class WindowsUnpacker
   end
 
   def sevenzip_command
-    if resource.strip_components > 0
-      require 'tmpdir'
-      tmpdir = Dir.mktmpdir
-      cmd = sevenzip_command_builder(tmpdir, 'e')
-      cmd += " && "
-      currdir = tmpdir
-      var = 0
-      while var < resource.strip_components
-        var += 1
-        cmd += "for /f %#{var} in ('dir /ad /b \"#{currdir.gsub! '/', '\\'}\"') do "
-        currdir += "\\%#{var}"
-      end
-      cmd += "xcopy \"#{currdir}\" \"#{resource.home_dir}\" /s /e"
-    else
-      cmd = sevenzip_command_builder(resource.path, 'x')
+    if resource.strip_components <= 0
+      sevenzip_command_builder(resource.path, 'x')
+      return
     end
-    cmd
+
+    require 'tmpdir'
+    tmpdir = Dir.mktmpdir
+    cmd = sevenzip_command_builder(tmpdir, 'e')
+
+    cmd += " && "
+    currdir = tmpdir.gsub('/', '\\')
+
+    1.upto(resource.strip_components).each do |count|
+      cmd += "for /f %#{count} in ('dir /ad /b \"#{currdir}\"') do "
+      currdir += "\\%#{count}"
+    end
+
+    cmd += "xcopy \"#{currdir}\" \"#{resource.home_dir}\" /s /e"
   end
 
-  def node
-    resource.run_context.node
+  def sevenzip_binary
+    resource.run_context.node['ark']['tar']
   end
 
   def sevenzip_command_builder(dir, command)
-    cmd = "#{node['ark']['tar']} #{command} \""
-    cmd += resource.release_file
-    cmd += "\" "
-    case resource.extension
-    when /tar.gz|tgz|tar.bz2|tbz|tar.xz|txz/
-      cmd += " -so | #{node['ark']['tar']} x -aoa -si -ttar"
+    "#{sevenzip_binary} #{command} \"#{resource.release_file}\"#{extension_is_tar} -o\"#{dir}\" -uy"
+  end
+
+  def extension_is_tar
+    if resource.extension =~ /tar.gz|tgz|tar.bz2|tbz|tar.xz|txz/
+      " -so | #{sevenzip_binary} x -aoa -si -ttar"
+    else
+      ""
     end
-    cmd += " -o\"#{dir}\" -uy"
-    cmd
   end
 end
+
 
 class WindowsDumper
   def initialize(resource)
