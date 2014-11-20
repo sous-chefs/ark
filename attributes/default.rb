bc02f75c21652c24c073c432be60576028c6c15d
Antek Baranski <antek.baranski@disney.com>
2014-11-19 17:42:52 -0800

    Fix path to tar on Mac OSX

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,10 +2,13 @@ default['ark']['apache_mirror'] = 'http://apache.mirrors.tds.net'
 default['ark']['prefix_root'] = '/usr/local'
 default['ark']['prefix_bin'] = '/usr/local/bin'
 default['ark']['prefix_home'] = '/usr/local'
-if node['platform_family'] == 'windows'
-  default['ark']['tar'] = "\"#{default['7-zip']['home']}\\7z.exe\""
-else
-  default['ark']['tar'] = '/bin/tar'
+case node['platform_family']
+  when 'windows'
+    default['ark']['tar'] = "\"#{default['7-zip']['home']}\\7z.exe\""
+  when 'mac_os_x'
+    default['ark']['tar'] = '/usr/bin/tar'
+  else
+    default['ark']['tar'] = '/bin/tar'
 end
 
 pkgs = %w(libtool autoconf) unless platform_family?('mac_os_x', 'windows')
