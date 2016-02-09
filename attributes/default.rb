fb1bd30e64e7b0df72bd71996b29273606f13b13
Tim Smith <tsmith84@gmail.com>
2016-02-08 23:17:27 -0800

    FreeBSD needs gmake not make

--- attributes/default.rb
+++ attributes/default.rb
@@ -32,9 +32,11 @@ default['ark']['tar'] = case node['platform_family']
                         end
 
 pkgs = %w(libtool autoconf) unless platform_family?('mac_os_x', 'windows')
-pkgs += %w(unzip rsync make gcc) unless platform_family?('mac_os_x', 'windows')
+pkgs += %w(make) unless platform_family?('mac_os_x', 'windows', 'freebsd')
+pkgs += %w(unzip rsync gcc) unless platform_family?('mac_os_x', 'windows')
 pkgs += %w(autogen) unless platform_family?('rhel', 'fedora', 'mac_os_x', 'suse', 'windows')
 pkgs += %w(gtar) if platform?('freebsd') || platform?('smartos')
+pkgs += %w(gmake) if platform?('freebsd')
 pkgs += %w(xz-lzma-compat bzip2 tar) if platform_family?('rhel', 'fedora')
 pkgs += %w(shtool pkg-config) if platform_family?('debian')
 
