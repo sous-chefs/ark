48671f5b907db1e252e8f4c8b034cff6836cab21
Tim Smith <tsmith84@gmail.com>
2016-02-08 21:59:25 -0800

    Install xz-lzma-compat and bzip2 on all RHEL platforms

--- attributes/default.rb
+++ attributes/default.rb
@@ -30,8 +30,7 @@ pkgs = %w(libtool autoconf) unless platform_family?('mac_os_x', 'windows')
 pkgs += %w(unzip rsync make gcc) unless platform_family?('mac_os_x', 'windows')
 pkgs += %w(autogen) unless platform_family?('rhel', 'fedora', 'mac_os_x', 'suse', 'windows')
 pkgs += %w(gtar) if platform?('freebsd')
-pkgs += %w(xz-lzma-compat bzip2) if platform?('centos')
-pkgs += %w(tar) if platform_family?('rhel', 'fedora')
+pkgs += %w(xz-lzma-compat bzip2 tar) if platform_family?('rhel', 'fedora')
 pkgs += %w(shtool pkg-config) if platform_family?('debian')
 
 default['ark']['package_dependencies'] = pkgs
