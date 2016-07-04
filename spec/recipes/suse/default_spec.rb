c2e30330f0ae9330d686b45fdcfe1461ed853bd4
Tim Smith <tsmith@chef.io>
2016-07-04 13:10:28 -0700

    Add suse spec
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ spec/recipes/suse/default_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe_recipe 'ark::default' do
+  def node_attributes
+    { platform: 'opensuse', platform_family: 'debian', version: '13.2' }
+  end
+
+  let(:expected_packages) do
+    %w( libtool autoconf unzip rsync make gcc )
+  end
+
+  it 'installs core packages' do
+    expected_packages.each do |package|
+      expect(chef_run).to install_package(package)
+    end
+  end
+end
