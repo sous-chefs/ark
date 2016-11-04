f72a7776422fc6eaf8a9352ce99844d00a61b81b
Tim Smith <tsmith@chef.io>
2016-11-03 21:02:59 -0700

    Fix suse support and centos < 6
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- attributes/default.rb
+++ attributes/default.rb
@@ -38,10 +38,10 @@ pkgs += %w(unzip rsync gcc) unless platform_family?('mac_os_x')
 pkgs += %w(autogen) unless platform_family?('rhel', 'fedora', 'mac_os_x', 'suse')
 pkgs += %w(gtar) if platform?('freebsd') || platform?('smartos')
 pkgs += %w(gmake) if platform?('freebsd')
-if platform_family?('rhel')
-  if node['platform_version'] >= '7'
+if platform_family?('rhel', 'suse')
+  if node['platform_version'].to_i >= 7
     pkgs += %w(xz bzip2 tar)
-  elsif node['platform_version'] < '7'
+  elsif node['platform_version'].to_i < 7
     pkgs += %w(xz-lzma-compat bzip2 tar)
   end
 elsif platform_family?('fedora')
