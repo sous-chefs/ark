e5c9770faa78818b560d935f9d0774955f4e3687
Tim Smith <tsmith84@gmail.com>
2019-12-12 16:58:14 -0800

    Remove long_description and recipe metadata
    These are both unused
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,7 @@ maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license          'Apache-2.0'
 description      'Provides a custom resource for installing runtime artifacts in a predictable fashion'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '4.0.0'
-
-recipe 'ark::default', 'Installs packages needed by the custom resource'
 
 %w(ubuntu debian redhat centos suse opensuse opensuseleap scientific oracle amazon windows mac_os_x smartos freebsd).each do |os|
   supports os
