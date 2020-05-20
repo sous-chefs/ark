23a182d48af42457ec7181cc58719396bf1b5035
Christophe Sourisse <christophe.sourisse@gmail.com>
2020-05-20 16:53:39 +0200

    Patching tests with broken access to haproxy.1wt.eu
    (when run by Travis)

--- spec/fixtures/cookbooks/ark_spec/recipes/install_with_make_with_clean_up_before_unpack.rb
+++ spec/fixtures/cookbooks/ark_spec/recipes/install_with_make_with_clean_up_before_unpack.rb
@@ -1,5 +1,5 @@
 ark 'test_install_with_make' do
-  url 'http://haproxy.1wt.eu/download/1.5/src/snapshot/haproxy-ss-20120403.tar.gz'
+  url 'https://github.com/dosyfier/ark/raw/develop/files/default/haproxy-ss-20120403.tar.gz'
   version '1.5'
   checksum 'ba0424bf7d23b3a607ee24bbb855bb0ea347d7ffde0bec0cb12a89623cbaf911'
   make_opts ['TARGET=linux26']
